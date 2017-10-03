9253e6f2c53094692d6820a2ef4d70cfd4f9c8cf
Rolrence <rolrence@qq.com>
2017-10-04 01:25:05 +0800

    [calculator] fix bugs in unit conversion

--- Calculator/app/src/main/java/rolrence/calculator/UnitConversionActivity.kt
+++ Calculator/app/src/main/java/rolrence/calculator/UnitConversionActivity.kt
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
 import kotlinx.android.synthetic.main.activity_unit_conversion.*
 import rolrence.calculator.converter.MeasureUnit
 
@@ -20,7 +22,7 @@ class UnitConversionActivity : AppCompatActivity() {
             val converter = bundle.getStringArray("converter")
 
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
+            imm.hideSoftInputFromWindow(window.decorView.windowToken, 0)
 
             listExchangeRate.adapter = ArrayAdapter<String>(
                     this@UnitConversionActivity,
@@ -32,15 +34,31 @@ class UnitConversionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_unit_conversion)
 
+        spinnerInputType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                spinnerInputUnit.adapter = ArrayAdapter<String>(
+                        this@UnitConversionActivity,
+                        android.R.layout.simple_spinner_item,
+                        when (spinnerInputType.selectedItem.toString()) {
+                            "长度" -> resources.getStringArray(R.array.length_unit_list)
+                            "面积" -> resources.getStringArray(R.array.area_unit_list)
+                            "重量" -> resources.getStringArray(R.array.weight_unit_list)
+                            else -> resources.getStringArray(R.array.length_unit_list)
+                        })
+            }
+        }
+
         btnConvert.setOnClickListener {
             Thread {
                 val input = txtInput.text.toString()
                 val inputType = spinnerInputType.selectedItem.toString()
+                val inputUnit = spinnerInputUnit.selectedItem.toString()
 
                 if (input != "") {
                     val msg = Message()
                     val bundle = Bundle()
-                    bundle.putStringArray("converter", MeasureUnit.list(input, inputType).map {
+                    bundle.putStringArray("converter", MeasureUnit.list(inputType, input, inputUnit).map {
                         "$input $inputType = ${it.value} ${it.key}"
                     }.toTypedArray())
                     msg.data = bundle
--- Calculator/app/src/main/java/rolrence/calculator/converter/MeasureUnit.kt
+++ Calculator/app/src/main/java/rolrence/calculator/converter/MeasureUnit.kt
@@ -13,13 +13,18 @@ class MeasureUnit {
                        val result: List<Item>)
 
         companion object {
-            fun get(value: String, unit: Int) = Gson().fromJson(
-                    AvatarData("http://api.avatardata.cn/UnitConvert/Weight?key=0c153a338b2a4f11aa5a0a575aa79c62&value=$value&unit=$unit")
+            fun get(type: String, value: String, unit: Int) = Gson().fromJson(
+                    AvatarData("http://api.avatardata.cn/UnitConvert/$type?key=0c153a338b2a4f11aa5a0a575aa79c62&value=$value&unit=$unit")
                             .get(), Msg::class.java)
         }
     }
 
     companion object {
+        val unitKindList = mapOf(
+                "长度" to "Length",
+                "面积" to "Area",
+                "重量" to "Weight"
+        )
         val unitList = mapOf(
                 "公里" to 1, "km" to 1,
                 "米" to 2, "m" to 2,
@@ -36,10 +41,46 @@ class MeasureUnit {
                 "英寸" to 13, "in" to 13,
                 "码" to 14, "yd" to 14,
                 "英尺" to 15, "ft" to 15,
-                "海里" to 16, "nmi" to 16
+                "海里" to 16, "nmi" to 16,
+
+                "平方公里" to 1,
+                "平方米" to 2,
+                "平方分米" to 3,
+                "平方厘米" to 4,
+                "平方毫米" to 5,
+                "平方英里" to 6,
+                "平方英尺" to 7,
+                "平方英寸" to 8,
+                "公顷" to 9,
+                "平方码" to 10,
+                "市亩" to 11,
+                "英亩" to 12,
+
+                "吨" to 1,
+                "公斤" to 2,
+                "克" to 3,
+                "毫克" to 4,
+                "市斤" to 5,
+                "担" to 6,
+                "两" to 7,
+                "钱" to 8,
+                "金衡磅" to 9,
+                "金衡盎司" to 10,
+                "英钱" to 11,
+                "金衡格令" to 12,
+                "长吨" to 13,
+                "短吨" to 14,
+                "英担" to 15,
+                "美担" to 16,
+                "英石" to 17,
+                "磅" to 18,
+                "盎司" to 19,
+                "打兰" to 20,
+                "格令" to 21
         )
 
-        fun list(value: String, unit: String) = Message.get(value, unitList[unit]!!).result.map {
+        fun list(type: String, value: String, unit: String) = Message.get(
+                unitKindList[type]!!, value, unitList[unit]!!).result.map {
             Pair(it.name, it.value)
         }.toMap()
 
